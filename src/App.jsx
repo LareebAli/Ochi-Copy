d8413084574f7e866eb2255b8bf09975064728ce
LareebAli <laraibalilaraib02@gmail.com>
2025-01-24 21:09:58 +0500

    hackathon

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import Navbar from './Component/Navbar'
-import Landingpgae from './Component/Landingpgae'
-import Maqee from './Component/Maqee'
-import About from './Component/About'
-import Eyes from './Component/Eyes'
-import Laptop from './Component/Laptop'
+
 const App = () => {
   return (
-    <div className=' w-full min-h-screen bg-white'>
-      <Navbar/>
-      <Landingpgae/>
-      <Maqee/>
-    <About/>
-    <Eyes/>
-    <Laptop/>
-    </div>
+    <div>laraib</div>
   )
 }
 
